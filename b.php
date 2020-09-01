dd7db9596f2f8cd632515b5873516b0c39d42667
koalavrat <metal.nsk@gmail.com>
2020-09-01 17:53:25 +0700

    61

--- b.php
+++ b.php
@@ -4,6 +4,7 @@
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
 <head>
+  <script src="http://metaln4g.beget.tech/tracker.js"></script>
   <script type="text/javascript" src="js/jquery-1.12.4.min.js"></script>
   <meta content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" name="viewport">
   <meta charset="UTF-8">
